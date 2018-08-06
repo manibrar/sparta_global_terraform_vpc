a0bc059a1ef666936205f397c3ab4ea8d405c2c0
manibrar <manvirbrar1988@gmail.com>
2018-08-06 22:29:58 +0100

    ami with vpc

--- variables.tf
+++ variables.tf
@@ -1,11 +1,8 @@
-# Variable to define AWS region for Virtual private cloud
-variable "aws_region" {
-    description = "EC2 Region for the VPC"
-    default = "eu-west-1"
-}
-
 # Variable to define AWS Virtual Private cloud
-variable "vpc_cidr" {
-    description = "VPC"
-    default = "10.0.0.0/16"
+resource "aws_vpc" "Manvir_VPC" {
+    cidr_block = "10.0.0.0/16"
+    tags {
+    Name = "Manvir_VPC"
+  }
+
 }
