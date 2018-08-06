8e74ff53945e4d1c6f838565e1df45290daa1c60
manibrar <manvirbrar1988@gmail.com>
2018-08-06 22:13:59 +0100

    vpc

--- variables.tf
+++ variables.tf
@@ -1,19 +1,11 @@
+# Variable to define AWS region for Virtual private cloud
 variable "aws_region" {
     description = "EC2 Region for the VPC"
     default = "eu-west-1"
 }
 
+# Variable to define AWS Virtual Private cloud
 variable "vpc_cidr" {
     description = "VPC"
     default = "10.0.0.0/16"
-}
-
-variable "public_subnet_cidr" {
-    description = "Public Subnet"
-    default = "10.0.0.0/24"
-}
-
-variable "private_subnet_cidr" {
-    description = "Private Subnet"
-    default = "10.0.1.0/24"
 }
